ce51c32a96831dc3d2ede910958c81b5287f8a89
Slurpd1 <104402459+Slurpd1@users.noreply.github.com>
2024-08-16 13:36:35 -0600

    completed excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(givenString, numOfLoops) {
+    // take a given string and loop it a certain amount of times
+    if (numOfLoops < 0) {
+        return "ERROR";
+    }
+    
+    let returnString = "";
 
+    for (let i = 0; i < numOfLoops; i++) {
+        returnString += givenString;
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
